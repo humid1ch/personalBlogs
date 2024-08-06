d74443497d3f9b91591fd235540adb64fd9ae054
DxytJuly3 <dxyt2002@163.com>
2024-08-06 20:47:17 +0800

    添加 八大排序分析文章 更新 界面信息与图标

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `July.cc Blogs`;
-export const SITE_DESCRIPTION = '七月.cc 的个人博客';
+export const SITE_TITLE = `humid1ch blogs`;
+export const SITE_DESCRIPTION = '哈米d1ch 的个人博客';
 export const SITE_EMAIL = 'dxyt2002@163.com'
 export const SITE_NAME = 'www.julysblog.cn';
 export const SITE_URL = "https://www.julysblog.cn";
@@ -9,4 +9,4 @@ export const SITE_NUM = "459,135";
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "July.cc";
+export const SITE_AUTHOR = "哈米d1ch";
